152077fb540afb3579a8c53ea24644505e603a8a
divyamzzz <divyam.singh.ug21@nsut.ac.in>
2024-07-07 01:53:30 +0530

    new changes

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,29 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const cheerio = require('cheerio');
-
-const app = express();
+const pg= require('pg');
+const helmet=require('helmet');
+const env=require("dotenv")
+const bcrypt=require("bcrypt")
+const passport=require("passport")
 const PORT = 5000;
-
 const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36';
-
+const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(helmet());
+env.config();
+const saltRounds=10;
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+  });
+  db.connect()
+    .then(() => console.log('Connected to the database'))
+    .catch(err => console.error('Database connection error', err.stack));
 
 async function fetchAmazonData(productName) {
     try {
@@ -98,6 +114,47 @@ app.get('/search', async (req, res) => {
         res.status(500).json({ error: 'An error occurred while fetching product data.' });
     }
 });
+
+
+// app.post('/login', async (req, res) => {
+//     const { username, password } = req.body;
+//     try {
+//       const result = await db.query('SELECT id from login where username=$1 and password=$2', [username,password]);
+//       if(result.rows.length===0)
+//         console.log("error");
+//       res.status(201).json(result.rows[0]);
+//     } catch (err) {
+//       console.error('Error executing query', err.stack);
+//       res.status(500).send('Server Error');
+//     }
+//   });
+  app.post('/signup', async (req, res) => {
+    const { name,username, password } = req.body;
+    try {
+      // Check if the user already exists
+      const checkUser = await db.query('SELECT * FROM login WHERE username = $1', [username]);
+      if (checkUser.rows.length > 0) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+      else
+      {
+        bcrypt.hash(password,saltRounds,async(err,hash)=>{
+            if(err)
+                console.log(err);
+            else{
+                const result = await db.query('INSERT INTO login (Name,username, password) VALUES ($1, $2,$3) RETURNING *', [name,username, hash]);
+      res.status(201).json(result.rows[0]);
+            }
+        })
+
+    } 
+  }
+  catch (err) {
+    console.error('Error executing query', err.stack);
+    res.status(500).send('Server Error');
+  }
+});
+  
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
